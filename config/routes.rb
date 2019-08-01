7b8113fce7756de9f02105f78d94c74da12e54ac
JUN1113 <dunkelrots@gmail.com>
2019-08-01 21:22:23 +0900

    setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "weblog#index"
-  resources :weblog
-  resources :user, only: [:show]
+  root "weblogs#index"
+  resources :weblogs
+  resources :users, only: [:show]
 end
