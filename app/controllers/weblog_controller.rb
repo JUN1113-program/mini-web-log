9e43dfd49c6a6cf70d231cc31c925877adbb2b6e
JUN1113 <dunkelrots@gmail.com>
2019-08-01 13:11:23 +0900

    create web controller for creating view files

--- app/controllers/weblog_controller.rb
+++ app/controllers/weblog_controller.rb
@@ -1,4 +1,18 @@
 class WeblogController < ApplicationController
   def index
   end
+
+  def new
+    
+  end
+
+  def create
+    redirect_to :index
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
