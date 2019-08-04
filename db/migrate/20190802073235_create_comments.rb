54f886d8ee7dc7571a27bc247a737efde188a63a
JUN1113 <dunkelrots@gmail.com>
2019-08-04 13:17:37 +0900

    fix migrate name comments to comment

--- db/migrate/20190802073235_create_comments.rb
+++ db/migrate/20190802073235_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.integer :rate
-      t.text :comments,null: false
+      t.text :comment,null: false
       t.references :weblog, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
