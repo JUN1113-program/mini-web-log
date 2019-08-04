63089c0714d4b7e9a4bc3008f53ec0663e45fa18
JUN1113 <dunkelrots@gmail.com>
2019-08-04 20:12:58 +0900

    add ranking controller

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,9 @@
+class RankingController < ApplicationController
+  before_action :rank
+  def rank
+    @viewed_rank = Weblog.order("viewed DESC").limit(3)
+    comment_nums = Comment.group(:weblog_id).order("count_weblog_id DESC").limit(3).count(:weblog_id).keys
+    @comment_rank = comment_nums.map{|num| Weblog.find(num)}
+    @random_rank = Weblog.order("rand()").limit(3)
+  end
+end
