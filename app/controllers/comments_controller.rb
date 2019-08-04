6655bede0db63672ab437cfc916214f2d7f569a1
JUN1113 <dunkelrots@gmail.com>
2019-08-04 13:11:50 +0900

    create comment create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to weblog_path(params[:weblog_id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id,weblog_id: params[:weblog_id])
+  end
 end
