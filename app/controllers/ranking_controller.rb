7235f6d0e9056c6ec085f728bb57ef8b9169d11e
JUN1113 <dunkelrots@gmail.com>
2019-09-16 18:28:54 +0900

    冗長なif文を三項演算子にまとめた

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,10 +4,7 @@ class RankingController < ApplicationController
     @viewed_rank = Weblog.order("viewed DESC").limit(3)
     comment_nums = Comment.group(:weblog_id).order("count_weblog_id DESC").limit(3).count(:weblog_id).keys
     @comment_rank = comment_nums.map{|num| Weblog.find(num)}
-    if Rails.env.production?
-      @random_rank = Weblog.order("RANDOM()").limit(3)
-    else
-      @random_rank = Weblog.order("rand()").limit(3)
-    end
+    rand = Rails.env.production? ? "RANDOM()" : "rand()"
+    @random_rank = Weblog.order(rand).limit(3)
   end
 end
