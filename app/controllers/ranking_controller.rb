ffdcc41ac9676347cb4a8a9f1bee7573476ece90
JUN1113 <dunkelrots@gmail.com>
2019-09-16 18:02:05 +0900

    randam機能を削除

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,6 +4,6 @@ class RankingController < ApplicationController
     @viewed_rank = Weblog.order("viewed DESC").limit(3)
     comment_nums = Comment.group(:weblog_id).order("count_weblog_id DESC").limit(3).count(:weblog_id).keys
     @comment_rank = comment_nums.map{|num| Weblog.find(num)}
-    @random_rank = Weblog.order("rand()").limit(3)
+    @random_rank = Weblog.limit(3)
   end
 end
