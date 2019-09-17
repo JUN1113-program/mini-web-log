d0b641d6deb4b858b973d5b13b69d50d5a9e077c
JUN1113 <dunkelrots@gmail.com>
2019-09-17 23:51:22 +0900

    自身の投稿以外を削除不可能に設定

--- app/controllers/weblogs_controller.rb
+++ app/controllers/weblogs_controller.rb
@@ -1,6 +1,8 @@
 class WeblogsController < RankingController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_weblog, only: [:edit, :update, :show, :destroy]
+  before_action :another_user?, only: :destroy
+
   def index
     @weblogs = Weblog.order("created_at DESC").page(params[:page]).per(10)
   end
@@ -11,7 +13,6 @@ class WeblogsController < RankingController
 
   def create
     @weblog = Weblog.create(weblog_params.merge(viewed: 0))
-    
     redirect_to root_path
   end
 
@@ -46,5 +47,9 @@ class WeblogsController < RankingController
 
   def weblog_params
     params.require(:weblog).permit(:title ,:content ,:image).merge(user_id: current_user.id)
+  end
+
+  def another_user?
+    redirect_to root_path unless current_user&.id == @weblog.user.id
   end
 end
