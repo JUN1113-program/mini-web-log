45da00efd9ac20efcd92e657b243174bc4953449
JUN1113 <dunkelrots@gmail.com>
2019-09-17 23:51:41 +0900

    titleにvaludatesを設定

--- app/models/weblog.rb
+++ app/models/weblog.rb
@@ -4,4 +4,5 @@ class Weblog < ApplicationRecord
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :content, presence: true
+  validates :title, presence: true
 end
