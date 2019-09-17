41e43ec8d87fed8d9cd2f11804e069c80173b41a
JUN1113 <dunkelrots@gmail.com>
2019-09-17 23:50:00 +0900

    コメントの作成をログインしているユーザーに制限

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
     Comment.create(comment_params)
     redirect_to weblog_path(params[:weblog_id])
