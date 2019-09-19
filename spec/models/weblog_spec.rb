41faf2d6e47b1c05d869e3667d36952f89473ef3
JUN1113 <dunkelrots@gmail.com>
2019-09-19 19:06:10 +0900

    記事を登録できる場合のテスト

--- spec/models/weblog_spec.rb
+++ spec/models/weblog_spec.rb
@@ -1,3 +1,21 @@
 require 'rails_helper'
 describe Weblog do
+  context "投稿できる場合" do
+    it "title, context, image, viewedが存在する場合" do
+      weblog = build(:weblog)
+      expect(weblog).to be_valid
+    end
+    it "title, context, imageが存在する場合" do
+      weblog = build(:weblog, viewed: "")
+      expect(weblog).to be_valid
+    end
+    it "title, context, viewedが存在する場合" do
+      weblog = build(:weblog, image: "")
+      expect(weblog).to be_valid
+    end
+    it "title, contextが存在する場合" do
+      weblog = build(:weblog, image: "", viewed: "")
+      expect(weblog).to be_valid
+    end
+  end
 end
