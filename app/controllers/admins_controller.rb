b1f0a7307b200c6df2b34f681662746b4e26b320
JUN1113 <dunkelrots@gmail.com>
2019-09-27 00:17:13 +0900

    adminへ直接URLを打たれた際の対策

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -12,6 +12,6 @@ class AdminsController < ApplicationController
 
   private
   def admin?
-    redirect_to root_path unless current_user.admin
+    redirect_to root_path unless current_user&.admin
   end
 end
