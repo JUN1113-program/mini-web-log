e7faf64feaf73d220b7aa213a104ee6d3aa965a3
JUN1113 <dunkelrots@gmail.com>
2019-09-26 23:52:19 +0900

    admin管理画面のコントローラ

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,10 @@
 class AdminsController < ApplicationController
   before_action :admin?
+
+  def index
+    @users = User.includes(:weblogs)
+  end
+
   private
   def admin?
     redirect_to root_path unless current_user.admin
