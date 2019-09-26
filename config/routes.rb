c918e665a622399738b1a3f1a140e8cf3a82a589
JUN1113 <dunkelrots@gmail.com>
2019-09-27 00:01:08 +0900

    ルーティングの記述ミスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "weblogs#index"
   resources :weblogs do
-    resources :comments, only: [:create,:delete]
+    resources :comments, only: [:create]
     collection do
       get 'search'
     end
   end
   resources :users, only: [:show]
-  resources :admins, only:[:index,:delete]
+  resources :admins, only:[:index,:destroy]
 end
