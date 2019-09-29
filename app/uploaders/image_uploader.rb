6a03ab4b13a612bfcea4c8ecffeafdbc19e89d4c
JUN1113 <dunkelrots@gmail.com>
2019-09-30 00:23:15 +0900

    投稿できるファイルのサイズを5mbまでに絞る

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,6 +37,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg png)
   end
 
+  def size_range
+    1..5.megabytes
+  end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
